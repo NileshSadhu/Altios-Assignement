9af0a83b1d1a37bc64280854b28db1dc910469e7
nileshsadhu562@gmail.com <nileshsadhu562@gmail.com>
2026-06-25 20:27:30 +0530

    added health api check route

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,9 +1,15 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
 import cors from "cors";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  return res.status(200).json({
+    message: "Health Check API is working fine.",
+  });
+});
 
 export default app;
