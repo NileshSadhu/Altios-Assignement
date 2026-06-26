8e576bca0b4d02c6a28f36bdb97e24db5551b8ed
nileshsadhu562@gmail.com <nileshsadhu562@gmail.com>
2026-06-26 13:28:57 +0530

    Fixed Validation bug

--- Server/src/validator/task.validator.ts
+++ Server/src/validator/task.validator.ts
@@ -3,7 +3,13 @@ import { PriorityStatus, TaskStatus } from "../models/task.model.js";
 
 export const taskSchema = z.object({
   title: z.string().trim().min(3).max(100),
-  description: z.string().trim().min(3).max(500).optional(),
+  description: z
+    .string()
+    .trim()
+    .max(500)
+    .optional()
+    .or(z.literal(""))
+    .transform((val) => (val === "" ? undefined : val)),
   status: z.nativeEnum(TaskStatus).optional(),
   priority: z.nativeEnum(PriorityStatus).optional(),
 });
