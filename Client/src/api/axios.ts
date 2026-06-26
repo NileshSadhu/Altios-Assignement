b1080891212ce497d8db83e2c1e14de6033c2b80
nileshsadhu562@gmail.com <nileshsadhu562@gmail.com>
2026-06-26 17:58:38 +0530

    still trying

--- Client/src/api/axios.ts
+++ Client/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    import.meta.env.SERVER_URL || "https://altois-assignement.onrender.com",
+    import.meta.env.SERVER_URL || "https://altois-assignement.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
